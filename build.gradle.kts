9b980132dc8595f4b2380376bf29f0a91a5d987c
Gurpreet3443 <gurp4915@gmail.com>
2025-04-22 19:45:09 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// build.gradle.kts (Project: ClosetMate)
+plugins {
+    id("com.android.application") version "8.8.0" apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
+    alias(libs.plugins.kotlin.android) apply false
+}
